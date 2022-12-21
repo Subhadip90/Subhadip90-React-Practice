e972f3a3acfb8c3ef9e9004d4b4b084112c61032
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-21 14:37:16 +0000

    image added with styling incss

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./index.css";
 
 ReactDOM.render(
   <>
-    <h1 contentEditable>Subhadip Chatterjee</h1>
+    <h1 contentEditable="true">Subhadip Chatterjee</h1>
+    <img src="https://images.pexels.com/photos/7948965/pexels-photo-7948965.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="dummy" style={{ height: "400px", width: "400px" }} />
   </>, document.getElementById("root"));
